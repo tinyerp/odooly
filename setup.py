850ad0af724c00f190cb97de0905830e0cf0bc00
Florent Xicluna <florent.xicluna@gmail.com>
2019-01-02 11:09:00 +0100

    Revert to unittest2py3k for Python 3.2 and 3.3

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+import sys
+
 from setuptools import setup
 
-setup()
+if (3, 0) < sys.version_info < (3, 4):
+    # Package 'unittest2' does not discover tests on Python 3.2 and 3.3
+    setup(
+        tests_require=['mock', 'unittest2py3k'],
+        test_suite='unittest2.collector.collector',
+    )
+else:
+    setup()
