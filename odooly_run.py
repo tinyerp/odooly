01ae41d19605806af9954fabc271bded3befce05
Florent Xicluna <florent.xicluna@gmail.com>
2026-03-17 20:17:40 +0100

    Refactor coloring function

--- odooly_run.py
+++ odooly_run.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 """Connect to an Odoo server."""
-# Colorize command-line output for Python <= 3.13
+# Colorize command-line output
 import builtins
 import keyword
 import io
 import os
-import pathlib
 import re
 import token as T
 import tokenize
@@ -13,11 +12,14 @@ import unicodedata
 
 import odooly
 
-BUILTINS = {str(name) for name in dir(builtins) if name[:1] != '_'}
+BUILTINS = {name for name in dir(builtins) if name[:1] != '_'}
 T_STRING = {getattr(T, tk) for tk in dir(T) if 'STRING' in tk}
 T_MIDDLE = {getattr(T, tk) for tk in dir(T) if 'STRING_MIDDLE' in tk}
-
-issoftkeyword = getattr(keyword, 'issoftkeyword', lambda t: False)
+KEYWORDS = {*keyword.kwlist}
+SOFTKEYW = {*getattr(keyword, 'softkwlist', ())}
+J_SPECIAL = {'false', 'null', 'true'}
+CONSTANTS = {'False', 'None', 'True'}
+DEF_CLASS = {'def', 'class'}
 
 
 class THEME:  # Default theme
@@ -31,22 +33,21 @@ class THEME:  # Default theme
     reset = '\x1b[0m'
 
 
-def gen_colors(value, theme=THEME()):
-    """Generate color tuples (start, end, color, reset_color)."""
+def gen_colors(value, theme, keywords=KEYWORDS):
+    """Generate color tuples (start, end, color)."""
     sio = io.StringIO(value)
-    line_lengths = [0] + [len(line) for line in sio.readlines()]
-    for i in range(1, len(line_lengths)):
-        line_lengths[i] += line_lengths[i-1]
-
+    line_offsets = [..., 0] + [sio.tell() for __ in sio]
     sio.seek(0)
+
     bracket_level = is_def_name = 0
+    yield None, 0, theme.reset
     try:
         for token in tokenize.generate_tokens(sio.readline):
             if token.start == token.end:
                 continue
-            color, end_offset = None, -1
+            color, end_offset = None, 0
             if token.type in T_STRING:
-                if token.type in T_MIDDLE and token.string.endswith(('{', '}')):
+                if token.type in T_MIDDLE and token.string[-1:] in '{}':
                     end_offset += 1
                 color = theme.string
             elif token.type == T.COMMENT:
@@ -60,53 +61,84 @@ def gen_colors(value, theme=THEME()):
                     bracket_level -= 1
                 color = theme.op
             elif token.type == T.NAME:
-                if is_def_name:
+                if token.string in keywords:
+                    is_def_name = token.string in DEF_CLASS
+                    color = theme.keyword
+                elif keywords is not KEYWORDS:
+                    continue
+                elif is_def_name:
                     is_def_name = False
                     color = theme.definition
-                elif keyword.iskeyword(token.string):
-                    if token.string in ('def', 'class'):
-                        is_def_name = True
-                    color = theme.keyword
-                elif issoftkeyword(token.string) and not bracket_level:
+                elif token.string in SOFTKEYW and not bracket_level:
                     color = theme.keyword  # soft_keyword
                 elif token.string in BUILTINS:
                     color = theme.builtin
             if color is not None:
-                start = line_lengths[token.start[0] - 1] + token.start[1]
-                end = line_lengths[token.end[0] - 1] + token.end[1] + end_offset
-                yield start, end, color, theme.reset
+                start = line_offsets[token.start[0]] + token.start[1]
+                end = line_offsets[token.end[0]] + token.end[1] + end_offset
+                yield start, end, color
     except (SyntaxError, tokenize.TokenError):
         pass
-    yield None, None, None, None
+    yield None, None, theme.reset
 
 
-def colorize(value):
-    """Colorize Python syntax."""
+def _apply_colors(value, theme, keywords):
     output = ''
     if value:
-        start, end, color, c_reset = next(colors := gen_colors(value))
+        __, end, c_reset = next(colors := gen_colors(value, theme=theme, keywords=keywords))
 
-        for idx, char in enumerate(value):
-            if start == idx:
-                output += color
-
-            if char > '\x7f' and unicodedata.category(char)[:1] == 'C':
-                char = rf'\u{ord(char):04x}'
-            output += char
-
-            if end == idx:
-                output += c_reset
-                start, end, color, c_reset = next(colors)
+        while (last_pos := end) is not None:
+            start, end, color = next(colors)
+            if start is None:
+                output += _escape(value[last_pos:])
+            else:
+                output += _escape(value[last_pos:start]) + color + _escape(value[start:end]) + c_reset
     return output
+
+
+def _escape(value):
+    if value.isascii():
+        return value
+    result = ''
+    for char in value:
+        if char > '\x7f' and unicodedata.category(char)[:1] == 'C':
+            char = rf'\u{ord(char):04x}'
+        result += char
+    return result
+
+
+def color_python(value):
+    """Colorize Python syntax."""
+    return _apply_colors(value, THEME(), KEYWORDS)
+
+
+def color_repr(value):
+    """Colorize string representation."""
+    return _apply_colors(value, THEME(), CONSTANTS)
+
+
+def color_json(value):
+    """Colorize JSON representation."""
+    return _apply_colors(value, THEME(), J_SPECIAL)
 
 
 def patch_colors(module):
     """Set functions to color output."""
-    module.colorize = colorize
-    module.color_bold = f'{THEME.definition}{{}}{THEME.reset}'.format
-    module.color_comment = f'{THEME.comment}{{}}{THEME.reset}'.format
+    global THEME
+    try:
+        from _pyrepl.utils import THEME
+    except ImportError:
+        pass
+
+    module.color_repr = color_repr
+    if module.color_py is not str:  # Python >= 3.14
+        return {'color_repr': color_repr}
+    theme = THEME()
+    module.color_py = color_python
+    module.color_bold = f'{theme.definition}{{}}{theme.reset}'.format
+    module.color_comment = f'{theme.comment}{{}}{theme.reset}'.format
     decolor = odooly.partial(re.compile(r'\x1b\[[;\d]+m').sub, '')
-    return {'colorize': colorize, 'decolor': decolor}
+    return {'color_py': color_python, 'color_repr': color_repr, 'decolor': decolor}
 
 
 def main():
@@ -119,10 +151,10 @@ def main():
         return
 
     global_vars = odooly.Client._set_interactive()
-    if os.getenv('FORCE_COLOR') and not os.getenv('NO_COLOR') and odooly.colorize is str:
-        global_vars.update(patch_colors(odooly))  # Python <= 3.13
+    if odooly.color_py is not str or (os.getenv('FORCE_COLOR') and not os.getenv('NO_COLOR')):
+        global_vars.update(patch_colors(odooly))
 
-    print(odooly.colorize(odooly.USAGE))
+    print(odooly.color_repr(odooly.USAGE))
     odooly.connect_client(args)
     odooly._interact(global_vars)
 
